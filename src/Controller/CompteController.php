a27dd80e0cdc49a475ef10e00379e1276e4fe069
anissabb <a.bojabah@interface3.be>
2023-04-04 22:12:59 +0200

    avis

--- src/Controller/CompteController.php
+++ src/Controller/CompteController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CompteController extends AbstractController
 {
-    #[Route('/compte', name: 'app_compte')]
-    public function index(): Response
+    #[Route('/compte/{id}', name: 'app_compte')]
+    public function index(ManagerRegistry $doctrine,$id): Response
     {
-        return $this->render('compte/index.html.twig', [
-            'controller_name' => 'CompteController',
+
+        
+        $repository=$doctrine->getRepository(User::class);
+
+        $user=$repository->find($id);
+        
+
+        
+        
+      
+        
+
+        return $this->render('compte/index.html.twig',[
+            'personne'=>$user,
         ]);
     }
 }
